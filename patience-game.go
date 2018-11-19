53d28d84ea9b063c9c0d4f3d8d9de86297a4efd0
Lucy <lucy@lucyleach.co.uk>
2018-11-19 20:14:52 +0000

    Simulate number of shuffles, check last card

--- patience-game.go
+++ patience-game.go
@@ -7,12 +7,12 @@ import (
 )
 
 type Card struct {
-	suit  string
-	value string
+	Suit  string
+	Value string
 }
 
 func (card Card) String() string {
-	return card.value + " of " + card.suit
+	return card.Value + " of " + card.Suit
 }
 
 var suits [4]string = [4]string{"Spades", "Clubs", "Diamonds", "Hearts"}
@@ -20,11 +20,21 @@ var values [13]string = [13]string{"Ace", "Two", "Three", "Four", "Five", "Six",
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
-	
+	numSimulations := 100000
+
 	deck := makeDeck()
-	shuffled := shuffledDeck(deck)
 	
-	fmt.Println("First card is: ", shuffled[0])
+	valueCount := make(map[string]int)
+	for i := 0; i < numSimulations; i++ {
+		shuffled := shuffledDeck(deck)
+		lastCard := shuffled[len(shuffled) - 1]
+		valueCount[lastCard.Value] = valueCount[lastCard.Value] + 1
+	}
+	
+	for _, value := range values {
+		fractionAppeared := float64(valueCount[value]) / float64(numSimulations)
+		fmt.Printf(value + ": %v\n", fractionAppeared)
+	}
 }
 
 func makeDeck() []Card {
